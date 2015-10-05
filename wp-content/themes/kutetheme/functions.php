4fcc2a9b3068656a9e11160804e2ffcff05b94a0
AngelsIT <thanhtd91@gmail.com>
2015-10-05 14:42:20 +0700

    Option 5

--- wp-content/themes/kutetheme/functions.php
+++ wp-content/themes/kutetheme/functions.php
@@ -327,6 +327,8 @@ if( ! function_exists( 'kt_scripts' ) ){
                 
         wp_enqueue_style( 'kt-option-6', get_template_directory_uri() . '/css/option6.css', array('kt-style') );
         
+        wp_enqueue_style( 'kt-option-5', get_template_directory_uri() . '/css/option5.css', array('kt-style') );
+        
         wp_enqueue_style( 'kt-option-2', get_template_directory_uri() . '/css/option2.css', array('kt-style') );
         
     	wp_enqueue_script( 'kt-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20141010', true );
--- wp-content/themes/kutetheme/templates/search-form/post-search-form.php
+++ wp-content/themes/kutetheme/templates/search-form/post-search-form.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-<form class="form-inline blog-search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ) ?>">
+<form class="form-inline <?php if( kt_is_wc() ) : ?> blog-search-form <?php endif; ?>"  method="get" action="<?php echo esc_url( home_url( '/' ) ) ?>">
       <div class="form-group input-serach">
         <?php if( kt_is_wc() ) : ?>
         <input type="hidden" name="post_type" value="product" />
